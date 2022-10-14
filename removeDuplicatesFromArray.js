dd1e5098a587dccc80cc8d274a68624b759e1592
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-10-15 02:11:55 +0500

    removeDuplicatesFromArray()

---
+++ removeDuplicatesFromArray.js
@@ -0,0 +1,25 @@
+// this is not the best case fro removing duplicates from an unsorted array
+
+let arr = [1,2,2,4,5,4];
+
+for(let i = 0; i < arr.length; i++){
+    for(let j = i+1; j < arr.length; j++){
+        if(arr[i] == arr[j]){
+            for(let k = j; k < arr.length; k++){
+                arr[k] = arr[k+1];
+            }
+        }
+    }
+}
+
+var count = 0;
+arr.forEach((elem, index) =>{
+    if(arr[index] == undefined) count++;
+})
+
+for(let i = 0; i < count; i++){ arr.pop(); }
+
+
+console.log(arr, " ", count);
+
+
