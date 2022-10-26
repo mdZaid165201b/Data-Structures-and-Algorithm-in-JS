14818ef93f1ba56d2729ff8d425dfd687efa463d
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-10-26 23:49:06 +0500

    Update checkAngram.js

--- checkAngram.js
+++ checkAngram.js
@@ -19,3 +19,18 @@ let checkAngram = (string1, string2) => {
 
 checkAngram("binary","bainyr") ? console.log("Angram!!!") : console.log("Not Angram!!!"); 
 checkAngram("zaid","zbbd") ? console.log("Angram!!!") : console.log("Not Angram!!!"); 
+
+
+let checkAngramSecond = (str1, str2) => {
+  if(str1.length !== str2.length){ return false; }
+  else{
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+    str1 = Array.from(str1).sort();
+    str2 = Array.from(str2).sort();
+    if(str1.join("") == str2.join("")){ return true; }
+    else{ return false; }
+  }
+  
+}
+console.log(checkAngramSecond("binary", "baniry"));
