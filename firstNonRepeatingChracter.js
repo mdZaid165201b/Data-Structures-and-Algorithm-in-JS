faefa0af253ccebc7adbaa5e65abdf1bab3843c3
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-11-01 01:06:39 +0500

    Create firstNonRepeatingChracter.js

---
+++ firstNonRepeatingChracter.js
@@ -0,0 +1,36 @@
+function firstNonRepeatingCharacter(str) {
+  let repeat = true;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      continue;
+    } else {
+      for (let j = 0; j < i; j++) {
+        if (str[i] !== str[j]) {
+          repeat = false;
+        } else {
+          continue;
+        }
+      }
+      if (repeat) {
+        repeat = false;
+        continue;
+      } else {
+        repeat = true;
+        for (let k = i + 1; k < str.length; k++) {
+          if (str[i] !== str[k]) {
+            repeat = false;
+          } else {
+            repeat = true;
+            break;
+          }
+        }
+        if (repeat) {
+          repeat = false;
+          continue;
+        } else {
+          return str[i];
+        }
+      }
+    }
+  }
+}
