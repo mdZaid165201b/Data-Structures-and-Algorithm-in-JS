a35761fcee3f6a457151c57a01cab47606521a93
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-11-24 20:40:32 +0500

    Update twoSumProblem.js

--- twoSumProblem.js
+++ twoSumProblem.js
@@ -1,11 +1,14 @@
 //For example, if the array is [3, 5, 2, -4, 8, 11] and the sum is 7, your program should return [[11, -4], [2, 5]] because 11 + -4 = 7 and 2 + 5 = 7.
 
 // time complexity for this approach is O(n^2);
-
-function findTwoSum(arr, sum){
+function twoSumProblem(arr, sum){
+  const pairArray = [];
   for(let i = 0; i < arr.length; i++){
     for(let j = i + 1; j < arr.length; j++){
-      arr[i] + arr[]
+      arr[i] + arr[j] === sum ? pairArray.push([arr[i],arr[j]]) : null;
     }
   }
+  console.log(...pairArray);
 }
+
+twoSumProblem([3, 5, 2, -4, 8, 11], 7);
