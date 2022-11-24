52fc7c755205fae6352b1574b42d37598cdf00ff
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-11-24 20:38:09 +0500

    Create twoSumProblem.js

---
+++ twoSumProblem.js
@@ -0,0 +1,3 @@
+//For example, if the array is [3, 5, 2, -4, 8, 11] and the sum is 7, your program should return [[11, -4], [2, 5]] because 11 + -4 = 7 and 2 + 5 = 7.
+
+// time complexity for this approach is O(n^2);
