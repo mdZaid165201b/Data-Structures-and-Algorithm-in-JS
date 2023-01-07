df0f82dda33187d0cbc008033a788830dc6f211e
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2023-01-07 19:50:16 +0500

    Update countNoOfstatues.js

--- countNoOfstatues.js
+++ countNoOfstatues.js
@@ -1,6 +1,11 @@
+function sortArray(array) {
+    return array.sort((a, b) =>{
+        return a - b;
+    })
+}
 function solution(statues) {
     let sortedArray = [...statues];
-    sortedArray.sort();
+    sortedArray = sortArray(statues);
     let count = 0;
     for(let i = sortedArray[0]; i <= sortedArray[sortedArray.length -1]; i++){
         if(!statues.includes(i)){ count++; } 
