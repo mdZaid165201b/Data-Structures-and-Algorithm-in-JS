4f4c034a2fd2e74521dfc330d9552e773c7a3055
mdZaid165201b <107930247+mdZaid165201b@users.noreply.github.com>
2023-06-25 22:53:22 +0500

    minor bugs fixed!!!

--- monthlyPayrollPeriods.js
+++ monthlyPayrollPeriods.js
@@ -7,17 +7,21 @@ const monthlyPayrollPeriods = (startDate, endDate) => {
     console.log(startDate, " ", endDate); 
     while(startDate <= endDate) {
         const days = daysInMonth(startDate.getMonth(), startDate.getFullYear());
+        console.log({days})
         const tempStartDate = new Date(startDate);
-        periods.push({month: months[tempStartDate.getMonth()], startDate: tempStartDate.getDate(), endDate: startDate.getDate() - 1});
         startDate.setMonth(startDate.getMonth() + 1);
-    }
-    console.log({periods}) 
+        periods.push({
+            month: months[tempStartDate.getMonth()],
+            startDate: `${tempStartDate.getDate()}/${months[tempStartDate.getMonth()]}/${tempStartDate.getFullYear()}`,
+            endDate: `${startDate.getDate()}/${months[startDate.getMonth()]}/${startDate.getFullYear()}`}); // startDate.getDate() - 1
+        }
+        // startDate.setMonth(startDate.getMonth() + 1);
+    console.log(periods) 
 }
 
 const daysInMonth = (month, year) => {
     const  days = new Date(year, month, 0).getDate();
-    console.log(days);
     return days;
 }
 
-monthlyPayrollPeriods("01/15/2023", "12/30/2023");
+monthlyPayrollPeriods("01/1/2023", "03/30/2024");
