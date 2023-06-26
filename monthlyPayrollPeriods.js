54946931b2200cc2606ae2e1715ebe4847d00aef
mdZaid165201b <107930247+mdZaid165201b@users.noreply.github.com>
2023-06-26 20:55:53 +0500

    monthlyPayrollPeriods bug fixed

--- monthlyPayrollPeriods.js
+++ monthlyPayrollPeriods.js
@@ -1,26 +1,46 @@
 const monthlyPayrollPeriods = (startDate, endDate) => {
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const periods = [];
-    console.log(startDate, " " ,endDate);
-    startDate = new Date(startDate);
-    endDate = new Date(endDate);
-    console.log(startDate, " ", endDate); 
-    while(startDate <= endDate) {
-        const days = daysInMonth(startDate.getMonth(), startDate.getFullYear());
-        console.log({days})
-        const tempStartDate = new Date(startDate);
-        startDate.setMonth(startDate.getMonth() + 1);
-        periods.push({
-            month: months[tempStartDate.getMonth()],
-            startDate: `${tempStartDate.getDate()}/${months[tempStartDate.getMonth()]}/${tempStartDate.getFullYear()}`,
-            endDate: `${startDate.getDate()}/${months[startDate.getMonth()]}/${startDate.getFullYear()}`}); // startDate.getDate() - 1
-        }
-    console.log(periods) 
-}
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const periods = [];
+  console.log(startDate, " ", endDate);
+  startDate = new Date(startDate);
+  endDate = new Date(endDate);
+  console.log(startDate, " ", endDate);
+  const currentDate = new Date(startDate);
+  while (currentDate <= endDate) {
+    const tempStartDate = new Date(currentDate);
+    const tempEndDate = new Date(currentDate);
+    console.log(tempStartDate.getMonth());
+    const monthDays = daysInMonth(
+      tempStartDate.getMonth() + 1,
+      tempStartDate.getFullYear()
+    );
+    tempEndDate.setDate(tempEndDate.getDate() + monthDays - 1);
+    currentDate.setDate(currentDate.getDate() + monthDays);
+    periods.push({
+      startDate: tempStartDate,
+      endDate: tempEndDate,
+      title: tempStartDate.toDateString() + " - " + tempEndDate.toDateString(),
+    });
+  }
+  console.log(periods);
+};
 
 const daysInMonth = (month, year) => {
-    const  days = new Date(year, month, 0).getDate();
-    return days;
-}
+  const days = new Date(year, month, 0).getDate();
+  return days;
+};
 
-monthlyPayrollPeriods("01/1/2023", "03/30/2024");
+monthlyPayrollPeriods("01/15/2023", "03/15/2024");
