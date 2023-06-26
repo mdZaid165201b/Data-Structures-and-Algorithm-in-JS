b17d295f28fcecf38395c16096dcf3e6029a6c5c
mdZaid165201b <107930247+mdZaid165201b@users.noreply.github.com>
2023-06-26 14:51:06 +0500

    flatten Object using closure!!!

---
+++ flattenObject.js
@@ -0,0 +1,31 @@
+const object = {
+    name: "zaid",
+    email: "muhammadzaidabcd@gmail.com",
+    website: "https//:zaiddev.netlify.app",
+    address: {
+        house: "XYZ",
+        street: "ABC",
+        city: "Lahore",
+        postalCode: "54000"
+    }
+}
+
+const flattenObject = () => {
+    const finalObject = {};
+    const internal = (object) => {
+        const objectKeyValue = Object.keys(object);
+        objectKeyValue.forEach((current, index) => {
+            if(typeof object[current] === "object") {
+                internal(object[current]);
+            }
+            else {
+                finalObject[current] = object[current];
+            }
+        })
+        return finalObject; 
+    }
+    return internal;
+}
+
+const flattenObjectFunction = flattenObject();
+console.log(flattenObjectFunction(object))
