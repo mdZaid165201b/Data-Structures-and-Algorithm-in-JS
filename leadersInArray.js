bb4f046c2a57dfb342dc289956f23c4695ba0b1c
mdZaid165201b <107930247+mdZaid165201b@users.noreply.github.com>
2023-07-10 06:17:38 +0500

    optimized leaders in array

--- leadersInArray.js
+++ leadersInArray.js
@@ -15,6 +15,21 @@ const leadersInArray = array => {
 
 }
 
+// optimized approach
+
+const leadersInArrayOptimized = array => {
+    let max = array[array.length - 1];
+    const leaders = [max]
+    for(let i = array.length - 1; i >= 0; i--) {
+        if(array[i] > max) {
+            max = array[i]
+            leaders.push(max)
+        }
+    }
+    console.log(leaders)
+}
+
 // const array = [16,17,4,3,5,2];
 const array = [1,2,3,4,0];
-console.log(leadersInArray(array))
+// console.log(leadersInArray(array))
+console.log(leadersInArrayOptimized(array))
