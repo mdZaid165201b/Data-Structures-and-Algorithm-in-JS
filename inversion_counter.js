1cf0ef86c1d7357f0b4e09e14c3034c99ca2cfcd
Muhammad Zaid <45224@gcslahore.edu.pk>
2023-12-23 23:01:15 +0500

    optimized_inversion_counter

--- inversion_counter.js
+++ inversion_counter.js
@@ -13,6 +13,35 @@ const inversionCounter = (array) => {
 };
 
 
+// optimized approach
+
+const optimized_inversion_counter = (array) => {
+    const map = new Map();
+    // populate map with array values as key value pairs
+    for(const index in array) {
+        map.set(array[index], { index, value: array[index] });
+    };
+
+    // check values in map and perform action accordingly
+    let count = 0;
+    let index = 0;
+    let temp = index;
+    while(index < array.length) {
+        if(temp === array.length - 1) {
+            index++
+            temp = index;
+            continue;
+        }
+        if(array[index] > map.get(array[temp + 1]).value && index < temp + 1) {
+            count++;
+            temp++;
+        }
+        else {
+            temp++;
+        }
+    }
+    return count;
+}
 
 
 const array = [2, 4, 1, 3, 5];
@@ -21,3 +50,4 @@ const array2 = [10, 10, 10];
 
 
 console.log(inversionCounter(array2));
+console.log(optimized_inversion_counter(array2));
